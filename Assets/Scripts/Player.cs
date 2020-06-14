69d25292f2e2cba829fa1692cb05f0dd9b2e8733
Unknown <edanuramail@gmail.com>
2020-06-14 10:39:53 -0400

    Jump working (not perfect)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class Player : MonoBehaviour
 {
@@ -23,9 +24,6 @@ public class Player : MonoBehaviour
 
     public GameObject groundRight;
     public GameObject groundLeft;
-
-    public float jumpForce;
-    public float speed;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -34,9 +32,10 @@ public class Player : MonoBehaviour
     
     void Update()
     {
-        Debug.Log("jumpNow is " +jumpNow);
         Jump();
     }
+
+    private float defaultJump;
 
     void Jump()
     {
@@ -44,9 +43,21 @@ public class Player : MonoBehaviour
         {
             if (onGround)
             {
+                if (thisGround == grounds.groundLeft)
+                {
+                    // go right
+                    float remainingDistance = Mathf.Abs(transform.position.x - groundRight.transform.position.x);
+                    totalJumpTime = remainingDistance / jumpSpeed;
+                    CalculateHeight(groundRight.transform, groundLeft.transform);
+                }
+                else if (thisGround == grounds.groundRight)
+                {
+                    //go left
+                    float remainingDistance = Mathf.Abs(transform.position.x - groundLeft.transform.position.x);
+                    totalJumpTime = remainingDistance / jumpSpeed;
+                    CalculateHeight(groundLeft.transform, groundRight.transform);
+                }
                 jumpNow = true;
-                Vector2 jumpSpeed = Vector2.up * jumpForce;
-                rb.AddForce(jumpSpeed);
             }
         }
 
@@ -54,22 +65,47 @@ public class Player : MonoBehaviour
         {
             if (thisGround == grounds.groundLeft)
             {
+                // go right
                 SwitchGrounds(groundRight.transform);
             }
             else if (thisGround == grounds.groundRight)
             {
+                //go left
                 SwitchGrounds(groundLeft.transform);
             }
         }
     }
 
+    public float heightModifier = 1;
+    void CalculateHeight(Transform target, Transform ground)
+    {
+        float diff = Mathf.Abs(ground.position.x - target.position.x);
+        jumpHeight = diff * heightModifier;
+        Debug.Log(jumpHeight);
+    }
+
+    //sag x arti
+    // sol x eksi
+    
+    float jumpHeight;
+    public float jumpSpeed = .1f;
+    private float totalJumpTime;
     void SwitchGrounds(Transform target)
     {
-        if (transform.position != target.position)
-        {
-            transform.position = Vector3.Lerp(transform.position, target.position, speed);
-        }
+        float remainingDistance = Mathf.Abs(transform.position.x - target.position.x);
+        float remainingTime = remainingDistance / jumpSpeed;
 
+        transform.position = Vector3.Lerp(transform.position, target.position, jumpSpeed * Time.deltaTime);
+
+        if (remainingTime > totalJumpTime / 2)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, jumpHeight, jumpSpeed*Time.deltaTime), 0);
+        }
+        else if (remainingTime > 0)
+        {
+            transform.position = new Vector3(transform.position.x,
+                Mathf.Lerp(transform.position.y, target.position.y, jumpSpeed*Time.deltaTime), 0);
+        }
     }
 
     enum grounds
